e366e3618bfb359c0e775471c2b414696d1a012a
sasha982025 <sasha982025@gmail.com>
2022-08-06 13:21:28 +0300

    revert image click

--- src/ImageResize.js
+++ src/ImageResize.js
@@ -1,9 +1,9 @@
-import defaultsDeep from 'lodash/defaultsDeep';
+import defaultsDeep from "lodash/defaultsDeep";
 import Quill from "quill";
-import DefaultOptions from './DefaultOptions';
-import { DisplaySize } from './modules/DisplaySize';
-import { Resize } from './modules/Resize';
-import { Toolbar } from './modules/Toolbar';
+import DefaultOptions from "./DefaultOptions";
+import { DisplaySize } from "./modules/DisplaySize";
+import { Resize } from "./modules/Resize";
+import { Toolbar } from "./modules/Toolbar";
 
 const knownModules = { DisplaySize, Toolbar, Resize };
 
@@ -13,11 +13,10 @@ const knownModules = { DisplaySize, Toolbar, Resize };
  * @see https://quilljs.com/blog/building-a-custom-module/
  */
 export default class ImageResize {
-
 	constructor(quill, options = {}) {
 		// save the quill reference and options
 		this.quill = quill;
-		this.body = document.getElementsByTagName('body')[0];
+		this.body = document.getElementsByTagName("body")[0];
 		// Apply the options to our defaults, and stash them for later
 		// defaultsDeep doesn't do arrays as you'd expect, so we'll need to apply the classes array from options separately
 		let moduleClasses = false;
@@ -34,15 +33,20 @@ export default class ImageResize {
 		}
 
 		// disable native image resizing on firefox
-		document.execCommand('enableObjectResizing', false, 'false');
+		document.execCommand("enableObjectResizing", false, "false");
 
 		// respond to clicks inside the editor
-		this.quill.root.addEventListener('click', this.handleClick, false);
-		this.quill.root.addEventListener('mscontrolselect', this.handleClick, false); //IE 11 support
-		this.quill.root.addEventListener('scroll', this.handleScroll, false);
+		this.quill.root.addEventListener("click", this.handleClick, false);
+		this.quill.root.addEventListener(
+			"mscontrolselect",
+			this.handleClick,
+			false
+		); //IE 11 support
+		this.quill.root.addEventListener("scroll", this.handleScroll, false);
 
-		this.quill.root.parentNode.style.position = this.quill.root.parentNode.style.position || 'relative';
-		this.body.addEventListener('click', this.handleClick, false);
+		this.quill.root.parentNode.style.position =
+			this.quill.root.parentNode.style.position || "relative";
+		this.body.addEventListener("click", this.handleClick, false);
 		// setup modules
 		this.moduleClasses = this.options.modules;
 
@@ -53,45 +57,46 @@ export default class ImageResize {
 		this.removeModules();
 
 		this.modules = this.moduleClasses.map(
-			ModuleClass => new (knownModules[ModuleClass] || ModuleClass)(this),
+			(ModuleClass) =>
+				new (knownModules[ModuleClass] || ModuleClass)(this)
 		);
 
-		this.modules.forEach(
-			(module) => {
-				module.onCreate();
-			},
-		);
+		this.modules.forEach((module) => {
+			module.onCreate();
+		});
 
 		this.onUpdate();
 	};
 
 	onUpdate = () => {
 		this.repositionElements();
-		this.modules.forEach(
-			(module) => {
-				module.onUpdate();
-			},
-		);
+		this.modules.forEach((module) => {
+			module.onUpdate();
+		});
 	};
 
 	removeModules = () => {
-		this.modules.forEach(
-			(module) => {
-				module.onDestroy();
-			},
-		);
+		this.modules.forEach((module) => {
+			module.onDestroy();
+		});
 
 		this.modules = [];
 	};
 
 	handleClick = (evt) => {
-		const isClickOnEditor = evt.path.find(el => el.className === 'ql-editor');
-		console.log('ImageResize ~ evt', isClickOnEditor);
-		if (!isClickOnEditor) {
-			this.hide();
-			return;
-		}
-		if (evt.target && evt.target.tagName && evt.target.tagName.toUpperCase() === 'IMG') {
+		// console.log("ImageResize ~ evt", evt);
+		// const isClickOnEditor = evt.path.find(
+		// 	(el) => el.className === "ql-editor"
+		// );
+		// if (!isClickOnEditor) {
+		// 	this.hide();
+		// 	return;
+		// }
+		if (
+			evt.target &&
+			evt.target.tagName &&
+			evt.target.tagName.toUpperCase() === "IMG"
+		) {
 			if (this.img === evt.target) {
 				// we are already focused on this image
 				return;
@@ -132,14 +137,14 @@ export default class ImageResize {
 		this.quill.setSelection(null);
 
 		// prevent spurious text selection
-		this.setUserSelect('none');
+		this.setUserSelect("none");
 
 		// listen for the image being deleted or moved
-		document.addEventListener('keyup', this.checkImage, true);
-		this.quill.root.addEventListener('input', this.checkImage, true);
+		document.addEventListener("keyup", this.checkImage, true);
+		this.quill.root.addEventListener("input", this.checkImage, true);
 
 		// Create and add the overlay
-		this.overlay = document.createElement('div');
+		this.overlay = document.createElement("div");
 		Object.assign(this.overlay.style, this.options.overlayStyles);
 
 		this.quill.root.parentNode.appendChild(this.overlay);
@@ -157,11 +162,11 @@ export default class ImageResize {
 		this.overlay = undefined;
 
 		// stop listening for image deletion or movement
-		document.removeEventListener('keyup', this.checkImage);
-		this.quill.root.removeEventListener('input', this.checkImage);
+		document.removeEventListener("keyup", this.checkImage);
+		this.quill.root.removeEventListener("input", this.checkImage);
 
 		// reset user-select
-		this.setUserSelect('');
+		this.setUserSelect("");
 	};
 
 	repositionElements = () => {
@@ -175,7 +180,9 @@ export default class ImageResize {
 		const containerRect = parent.getBoundingClientRect();
 
 		Object.assign(this.overlay.style, {
-			left: `${imgRect.left - containerRect.left - 1 + parent.scrollLeft}px`,
+			left: `${
+				imgRect.left - containerRect.left - 1 + parent.scrollLeft
+			}px`,
 			top: `${imgRect.top - containerRect.top + parent.scrollTop}px`,
 			width: `${imgRect.width}px`,
 			height: `${imgRect.height}px`,
@@ -190,10 +197,10 @@ export default class ImageResize {
 
 	setUserSelect = (value) => {
 		[
-			'userSelect',
-			'mozUserSelect',
-			'webkitUserSelect',
-			'msUserSelect',
+			"userSelect",
+			"mozUserSelect",
+			"webkitUserSelect",
+			"msUserSelect",
 		].forEach((prop) => {
 			// set on contenteditable element and <html>
 			this.quill.root.style[prop] = value;
@@ -212,24 +219,22 @@ export default class ImageResize {
 }
 
 if (window.Quill) {
-
 	//BEGIN allow image alignment styles
-	const ImageFormatAttributesList = [
-		'alt',
-		'height',
-		'width',
-		'style',
-	];
+	const ImageFormatAttributesList = ["alt", "height", "width", "style"];
 
-	var BaseImageFormat = window.Quill.import('formats/image');
+	var BaseImageFormat = window.Quill.import("formats/image");
 	class ImageFormat extends BaseImageFormat {
 		static formats(domNode) {
-			return ImageFormatAttributesList.reduce(function (formats, attribute) {
+			return ImageFormatAttributesList.reduce(function (
+				formats,
+				attribute
+			) {
 				if (domNode.hasAttribute(attribute)) {
 					formats[attribute] = domNode.getAttribute(attribute);
 				}
 				return formats;
-			}, {});
+			},
+			{});
 		}
 		format(name, value) {
 			if (ImageFormatAttributesList.indexOf(name) > -1) {
@@ -247,13 +252,14 @@ if (window.Quill) {
 	window.Quill.register(ImageFormat, true);
 	//END allow image alignment styles
 
-
 	//Add support for IE 11
-	if (typeof Object.assign != 'function') {
+	if (typeof Object.assign != "function") {
 		Object.assign = function (target) {
-			'use strict';
+			"use strict";
 			if (target == null) {
-				throw new TypeError('Cannot convert undefined or null to object');
+				throw new TypeError(
+					"Cannot convert undefined or null to object"
+				);
 			}
 
 			target = Object(target);
@@ -271,5 +277,5 @@ if (window.Quill) {
 		};
 	}
 
-	window.Quill.register('modules/imageResize', ImageResize);
+	window.Quill.register("modules/imageResize", ImageResize);
 }
